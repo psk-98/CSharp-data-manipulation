7f324fd57bb860479d3ee761b8c74a92d3961847
omen <omen@pop-os.localdomain>
2024-02-20 15:13:55 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+//Composite Formatting 
+// string first = "Hello";
+// string second = "World";
+// Console.WriteLine("{1} {0}!", first, second);
+// Console.WriteLine("{0} {0} {0}!", first, second);
+// Console.WriteLine("\n\n");
+
+//String interpolation
+// string first = "Hello";
+// string second = "World";
+// Console.WriteLine($"{first} {second}!");
+// Console.WriteLine($"{second} {first}!");
+// Console.WriteLine($"{first} {first} {first}!");
+
+//Formatting currncy
+
+// decimal price = 123.45m;
+// int discount = 50;
+// Console.WriteLine($"Price: {price:C} (Save {discount:C})");
+// Console.WriteLine("\n\n");
+
+
+//Formating numbers 
+
+decimal measurement = 123456.78912m;
+Console.WriteLine($"Measurement: {measurement:N} units\n");
+
+decimal measurement2 = 123456.78912m;
+Console.WriteLine($"Measurement: {measurement2:N4} units\n\n");
+
+
+//Formating percentages 
+decimal tax = .36785m;
+Console.WriteLine($"Tax rate: {tax:P2}");
+
+decimal price = 67.55m;
+decimal salePrice = 59.99m;
+
+string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} price. ", (price - salePrice), price);
+
+yourDiscount += $"A discount of {((price - salePrice) / price):P2}!"; //inserted
+Console.WriteLine(yourDiscount);
+
+string input = "Pad this";
+Console.WriteLine(input.PadLeft(12));
+Console.WriteLine(input.PadLeft(12, '-'));
+Console.WriteLine(input.PadRight(12, '-'));
